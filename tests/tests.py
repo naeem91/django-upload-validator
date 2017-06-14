2104b6311b723734a0de3981f4624be1ff7f1fec
Naeem Ilyas <naeem-ilyas@live.com>
2017-06-14 07:10:45 +0000

    more test files added

--- tests/tests.py
+++ tests/tests.py
@@ -9,6 +9,20 @@ from upload_validator import FileTypeValidator
 TEST_FILES_DIR = os.path.join(os.path.dirname(__file__), 'test_files')
 
 
+TEST_FILES = [
+    # word
+    'word-mac.doc', 'word-mac.docx', 'word-windows.docx', 'word-ubuntu.doc', 'word-ubuntu.docx',
+    # excel
+    'excel-mac.xls', 'excel-mac.xlsx', 'excel-windows.xlsx', 'excel-ubuntu.xls', 'excel-ubuntu.xlsx', 'document.xlsx',
+    # power point
+    'sample.ppt', 'sample.pptx',
+    # pdf
+    'sample.pdf',
+    # images
+    'sample.png', 'sample.jpeg', 'sample.tif',
+]
+
+
 @ddt
 class TestFileValidator(TestCase):
     def test_initialization(self):
@@ -21,8 +35,7 @@ class TestFileValidator(TestCase):
 
         self.assertTrue(isinstance(validator, FileTypeValidator))
 
-    @data('sample.doc', 'sample.pdf', 'sample.docx', 'sample.pptx', 'sample.ppt',
-          'sample.tif', 'sample.jpeg', 'sample.png', 'sample.xls', 'sample.xlsx')
+    @data(*TEST_FILES)
     def test_valid_types(self, filename):
         """
         Tests that different files are detected correctly
